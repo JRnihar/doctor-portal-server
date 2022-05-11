95c2195da349a62b31f1546791e33fc83ea812f7
JRnihar <niharmonirulislam@gmail.com>
2022-05-11 23:59:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+app.use(cors());
+app.use(express.json())
+
+require('dotenv').config()
+
+// const collection = client.db("test").collection("devices");
+
+const uri = "mongodb+srv://doctor:TFv7DfmM5J0bz1KZ@cluster0.dctmt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('doctors_portal').collection('services');
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Running my node CRUD server')
+})
+
+app.listen(port, () => {
+    console.log('crud server is running ');
+})
